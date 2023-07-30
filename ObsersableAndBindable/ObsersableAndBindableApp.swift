fb71e95082556c99e1bb445d7d052884420a90e5
David Grammatico <david.lugdunum@gmail.com>
2023-07-30 13:44:39 +0100

    Initial Commit

---
+++ ObsersableAndBindable/ObsersableAndBindableApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObsersableAndBindableApp.swift
+//  ObsersableAndBindable
+//
+//  Created by David Grammatico on 30/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ObsersableAndBindableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
